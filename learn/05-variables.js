69d1373e1b1c756f047a8c2e890d3c7024f9c357
mjnji-kim0524 <always0524@naver.com>
2025-06-10 20:47:13 +0900

    [학습] JavaScript 변수 - Variables - let 키워드로 선언 - 의미를 부여 - 값 할당
    [학습] JavaScript 변수 이름 작성 규칙
    - 공백, 숫자, 하이픈, 예약어 사용 X
    - 의미있는 이름 작성 권장
    
    [학습] JavaScript 변수(변할 수 있는)
    - 시간의 흐름에 따라 값이 변할 수 있음
    - 변수 선언
    - 선언된 변수에 값 할당
    
    [학습] 변수 vs. 상수
    - let, var 변수
    - const 상수

--- learn/05-variables.js
+++ learn/05-variables.js
@@ -19,20 +19,18 @@ console.log('처음_가진_사과의_갯수 = ', 처음_가진_사과의_갯수)
 // console.log('처음_가진_사과의_갯수 = ', 처음_가진_사과의_갯수) // 4
 // let initial_apples = 4 // snakeCase
 // console.log('initial_apples =', initial_apples)
-
-
-let initialApples = 4 //camelCase
-console.log('initialApples =', initialApples)
+let $initialApples = 4 //camelCase
+console.log('$initialApples =', $initialApples)
 
 // 잘못된 변수 이름
-// 공백은 안돼요!
-// let initial apples [x]
-// 첫 글자가 숫자 안돼요!
-// let 4people [x]
+// 공백(' ')은 안돼요!
+// let initial apples [❌]
+// 첫글자가 숫자 안돼요!
+// let 4people = '4명의 친구들' [❌]
 // 하이픈(-) 기호 안돼요!
-// let dart-impact = '다크 임팩트'
-// 예약어(예: if, for, while, fuction, ...)사용 안돼요!
-// let return = '회귀'
+// let dark-impact = '다크 임팩트' [❌]
+// 예약어(예: if, for, while, function, ...) 안돼요!
+// let return = '회귀' [❌]
 
 // 추가로 구매한 사과의 갯수 = 27
 // let 추가로_구매한_사과의_갯수 = 27
@@ -50,6 +48,8 @@ console.log('initialApples =', initialApples)
 // console.log('현재_내가_가진_사과의_갯수 = ', 현재_내가_가진_사과의_갯수) // 4 + 27
 // let 현재_내가_가진_사과의_갯수 = 처음_가진_사과의_갯수 + 추가로_구매한_사과의_갯수
 // console.log('현재_내가_가진_사과의_갯수 = ', 현재_내가_가진_사과의_갯수) // 4 + 27
+
+// let 변수 vs. var 변수
 
 
 // 블록 스코프(block scope)
@@ -75,6 +75,7 @@ console.log(3, myName)
 // myName2 = 'y9'
 // console.log(3, myName2)
 
+// ----------------------------------------------
 // let 변수 vs. const 변수(상수, constant)
 // - 변수 = 시간의 흐름에 따라 값이 변경됨
 // - 상수 = 처음 선언 시, 할당된 값이 이후 변경되지 않음
