2666aada393ec6d730f2b4ac0d313237d06c030c
mjnji-kim0524 <always0524@naver.com>
2025-06-10 20:50:34 +0900

    [실습] 변수 및 상수 이름 작성 (의미 부여) - 변하지 않는 것 - 변하는 것

--- learn/06-practice.js
+++ learn/06-practice.js
@@ -4,12 +4,23 @@
 //     1. "하루는 24시간"
 const HOURS_IN_DAY = 24
 console.log('HOURS_IN_DAY =', HOURS_IN_DAY)
+// HOURS_IN_DAY = 25 ❌
+
 //     2. "물의 어는 점은 섭씨 0도"
-const frozenTemperature = 0
+// FREEZING_POINT_CELSIUS
+const FREEZING_POINT_CELSIUS = 0
+console.log('FREEZING_POINT_CELSIUS =', FREEZING_POINT_CELSIUS)
+// FREEZING_POINT_CELSIUS = -20 ❌
+
 //     3. "삼각형 내각의 합은 180도"
-const sumTriangleDegree = 180
+const TRIANGLE_ANGLE_SUM = 180;
+console.log("TRIANGLE_ANGLE_SUM =", TRIANGLE_ANGLE_SUM);
+
 //     4. "지구의 중력 가속도는 9.8 m/s²"
-const earthGravity = 9.8
+const EARTH_GRAVITY = "9.8 m/s²";
+console.log("EARTH_GRAVITY =", EARTH_GRAVITY);
+
+
 // 2. 다음 내용을 코드로 작성해 결과 값을 콘솔 패널에 출력해보세요.
 //     1. `let` 키워드를 사용해 `sum` 변수를 선언하고 값을 `0`으로 설정합니다.
 let sum = 0
@@ -31,12 +42,14 @@ console.log(num2)
 // let num1 = 300, num2 = 50
 
 // [case4]
-// let num1 = 300
-//     num2 = 50
+// let num1 = 300, 
+//     num2 = 50,
+//     num3 = -90
 
 //     3. `num1`, `num2`의 합을 `sum` 변수에 할당합니다.
 sum = num1 + num2
-console.log(sum)
+console.log("sum =", sum)
+
 // 3. 사람 이름, 나이, 오늘의 기온, 장바구니 상품 갯수, 집 주소, 현재 시간,
 // 로그인 상태, 피자 가격, 학생 수 등에 걸맞는 변수 이름을 정하고 적절한 값을 설정합니다.
 
@@ -47,17 +60,20 @@ let personName = '김민지'
 let age = 36
 
 // 오늘의 기온
-let todayTemperature = '30도'
+let todayTemperature = '🌡️ 30도'
 console.log('todayTemperature =', todayTemperature)
 
 // 장바구니 상품 갯수
 let cartItemCount = 12
+console.log('cartItemCount =', cartItemCount)
 
 // 집 주소
 let homeAddress = '서울특별시 성북구 미아동 ...'
+console.log('homeAddress =', homeAddress)
 
 // 현재 시간
 let currentTime = '오후 2:37'
+console.log('currentTime =', currentTime)
 
 // 로그인 상태
 let isLoggedIn = false
@@ -68,6 +84,8 @@ console.log('isLoggedIn =', isLoggedIn)
 
 // 피자 가격
 let pizzaPrice = 22_000
+console.log('pizzaPrice =', pizzaPrice)
 
 // 학생 수
 let studentCount = 43
+console.log('studentCount =', studentCount)
